1d6bcb9e32dd842c379d5d6a8852ac64e8cc7226
ChrisKnud <130773062+ChrisKnud@users.noreply.github.com>
2023-04-15 01:08:15 +0200

    Update ex.py

--- ProbabilityDistributionsPy/ex.py
+++ ProbabilityDistributionsPy/ex.py
@@ -4,14 +4,8 @@ import math
 def beta_ex(a, b):
     return print("E[X]: ", a / (a + b))
 
-    # Works
-
-
 def gamma_ex(shape, scale):
     return print("E[X]: ", shape / scale)
-
-    # Works
-
 
 def nb_ex(n, p):
     return print("E[X]: ", n * (1 - p) / p)
